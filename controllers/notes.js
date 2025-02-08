6a184dab2abc6131669501b615ac06d3b99869dd
Julian Simon <juedsay@gmail.com>
2025-02-08 12:39:10 -0300

    Agregar enrutadores para usuarios y notas, refactorizar la lógica de manejo de notas a un controlador separado

---
+++ controllers/notes.js
@@ -0,0 +1,87 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/Note')
+
+notesRouter.get('/api/notes', async (req, res) => {
+    const notes = await Note.find({});
+    res.json(notes);
+});
+
+notesRouter.get('/api/notes/:id', async (req, res, next) => {
+    const { id } = req.params;
+  
+    Note.findById(id)
+      .then((note) => {
+        if (note) {
+          return res.json(note);
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch((error) => next(error));
+  });
+
+  notesRouter.put("/api/notes/:id", async (req, res, next) => {
+    const { id } = req.params;
+    const note = req.body;
+  
+    const newNoteInfo = {
+      content: note.content,
+      important: note.important,
+    };
+  
+    Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((error) => next(error));
+  })
+
+  notesRouter.delete("/api/notes/:id", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: 'Invalid ID format'});
+      }
+  
+      const note = await Note.findByIdAndDelete(id);
+      if (!note) {
+        return res.status(404).json({ error: 'Note not found'});
+      }
+  
+      res.status(204).end()    
+    } catch (error) {
+      next(error);    
+    };
+  });
+
+  notesRouter.post("/api/notes", async (req, res) => {
+    const note = req.body;
+  
+    if (!note || !note.content) {
+      return res.status(400).json({
+        error: "note.content is missing",
+      });
+    }
+  
+    const newNote = new Note({
+      content: note.content,
+      date: new Date(),
+      important: note.important || false,
+    });
+  
+    //   newNote.save().then((savedNote) => {
+    //     res.json(savedNote);
+    //   });
+  
+    try {
+      const savedNote = await newNote.save();
+      res.json(savedNote);
+      
+    } catch (error) {
+      next(error);    
+    };
+  
+  });
+
+module.exports = notesRouter;
--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const notFound = require("./middleware/notFound");
 const handleErrors = require("./middleware/handleErrors");
 const { default: mongoose } = require("mongoose");
 
+const usersRouter = require('./controllers/users')
+const notesRouter = require('./controllers/notes')
+
 app.use(express.json()); // Para realizar el parseo de los datos en formato JSON
 
 app.use(cors());
@@ -20,88 +23,91 @@ app.get("/", (req, res) => {
   res.send("<h1>Home Sweet Home</h1>");
 });
 
-app.get("/api/notes", async (req, res) => {
-  const notes = await Note.find({});
-  res.json(notes);
-});
+// app.get("/api/notes", async (req, res) => {
+//   const notes = await Note.find({});
+//   res.json(notes);
+// });
 
-app.get("/api/notes/:id", (req, res, next) => {
-  const { id } = req.params;
+// app.get("/api/notes/:id", (req, res, next) => {
+//   const { id } = req.params;
 
-  Note.findById(id)
-    .then((note) => {
-      if (note) {
-        return res.json(note);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
+//   Note.findById(id)
+//     .then((note) => {
+//       if (note) {
+//         return res.json(note);
+//       } else {
+//         res.status(404).end();
+//       }
+//     })
+//     .catch((error) => next(error));
+// });
 
-app.put("/api/notes/:id", (req, res, next) => {
-  const { id } = req.params;
-  const note = req.body;
+// app.put("/api/notes/:id", (req, res, next) => {
+//   const { id } = req.params;
+//   const note = req.body;
 
-  const newNoteInfo = {
-    content: note.content,
-    important: note.important,
-  };
+//   const newNoteInfo = {
+//     content: note.content,
+//     important: note.important,
+//   };
 
-  Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((error) => next(error));
-});
+//   Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
+//     .then((result) => {
+//       res.json(result);
+//     })
+//     .catch((error) => next(error));
+// });
 
-app.delete("/api/notes/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
+// app.delete("/api/notes/:id", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ error: 'Invalid ID format'});
-    }
+//     if (!mongoose.Types.ObjectId.isValid(id)) {
+//       return res.status(400).json({ error: 'Invalid ID format'});
+//     }
 
-    const note = await Note.findByIdAndDelete(id);
-    if (!note) {
-      return res.status(404).json({ error: 'Note not found'});
-    }
+//     const note = await Note.findByIdAndDelete(id);
+//     if (!note) {
+//       return res.status(404).json({ error: 'Note not found'});
+//     }
 
-    res.status(204).end()    
-  } catch (error) {
-    next(error);    
-  };
-});
+//     res.status(204).end()    
+//   } catch (error) {
+//     next(error);    
+//   };
+// });
 
-app.post("/api/notes", async (req, res) => {
-  const note = req.body;
+// app.post("/api/notes", async (req, res) => {
+//   const note = req.body;
 
-  if (!note || !note.content) {
-    return res.status(400).json({
-      error: "note.content is missing",
-    });
-  }
+//   if (!note || !note.content) {
+//     return res.status(400).json({
+//       error: "note.content is missing",
+//     });
+//   }
 
-  const newNote = new Note({
-    content: note.content,
-    date: new Date(),
-    important: note.important || false,
-  });
+//   const newNote = new Note({
+//     content: note.content,
+//     date: new Date(),
+//     important: note.important || false,
+//   });
 
-  //   newNote.save().then((savedNote) => {
-  //     res.json(savedNote);
-  //   });
+//   //   newNote.save().then((savedNote) => {
+//   //     res.json(savedNote);
+//   //   });
 
-  try {
-    const savedNote = await newNote.save();
-    res.json(savedNote);
+//   try {
+//     const savedNote = await newNote.save();
+//     res.json(savedNote);
     
-  } catch (error) {
-    next(error);    
-  };
+//   } catch (error) {
+//     next(error);    
+//   };
 
-});
+// });
+
+app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(notFound);
 
