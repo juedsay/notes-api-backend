81f5ecd4057b28685a65678cca3f05d5b8f4d225
Julian Simon <juedsay@gmail.com>
2025-02-08 09:46:33 -0300

    Actualizar pruebas para agregar y validar notas en la API

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -72,6 +72,46 @@ test("Second note must'n be important", async () => {
     expect(res.body[1].important).toEqual(false)      
 });
 
+test("a valid note can be added", async () =>{
+    //Note I'll try to post
+    const newNote = {
+        content: 'A new note has be added with success',
+        important: true
+    }
+    
+    //Calling a service to post a newNote
+    await api.post('/api/notes')
+    .send(newNote)
+    .expect(200)
+    .expect('Content-type', /application\/json/)
+
+    //Validating a newNote was created successfully, calling a service to get all created notes
+    const res = await api.get('/api/notes')
+
+    const contents = res.body.map(note => note.content)
+
+    expect(res.body).toHaveLength(initialNotes.length + 1)
+    expect(contents).toContain(newNote.content)
+})
+
+test("a note with out content can't be added", async () =>{
+    //New note I'll try to post
+    const newNote = {
+        important: true
+    }
+    
+    //Calling a service to post a newNote
+    await api.post('/api/notes')
+    .send(newNote)
+    .expect(400)
+    .expect('Content-type', /application\/json/)
+
+    //Validating a newNote was't created, calling a service to get all created notes
+    const res = await api.get('/api/notes')
+
+    expect(res.body).toHaveLength(initialNotes.length)
+})
+
 afterAll(() => {
     mongoose.connection.close()
     server.close()
